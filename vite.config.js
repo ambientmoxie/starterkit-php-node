858c8195761a41d5b251d1a7e2500289951b06bb
ambientmoxie <contact@maximebenoit.work>
2025-04-15 09:55:50 +0200

    Merge: 3 vite config files into one

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
 import { defineConfig } from "vite";
 import dotenv from "dotenv";
+import fullReload from "vite-plugin-full-reload";
 
 dotenv.config();
 
+const isDev = process.env.VITE_ENV_MODE === "dev";
+const isHost = process.env.VITE_ENV_MODE === "host";
+
 export default defineConfig({
   root: ".",
-  base: process.env.VITE_DEV == "true" ? "" : "/assets/bundle",
+  base: isDev || isHost ? "" : "/assets/bundle",
+  server: {
+    host: isHost ? true : "localhost",
+    origin: isHost
+      ? `http://${process.env.VITE_LOCAL_IP}:3000`
+      : "http://localhost:3000",
+    port: 3000,
+    hot: true,
+  },
+  plugins: [
+    fullReload(["**/*.php"]),
+  ],
   build: {
     rollupOptions: {
       input: "./assets/js/main.js",
