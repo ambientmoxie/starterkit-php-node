8ab320743bd6b9365e2d4303b6e8c4346a87760d
ambientmoxie <contact@maximebenoit.work>
2025-04-15 09:35:30 +0200

    Add: Watch mechanism for .php files

--- index.php
+++ index.php
@@ -24,6 +24,7 @@
 
 <body>
     <?php echo $_ENV['VITE_DEV'] ?>
+    <p>yo</p>
 </body>
 
 </html>
--- vite.host.config.js
+++ vite.host.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
+import fullReload from "vite-plugin-full-reload";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export default defineConfig({
   root: ".",
+  plugins: [
+    fullReload(['**/*.php']),
+  ],
   server: {
     host: true,
     origin: `http://${process.env.VITE_DEV}:3000`,
