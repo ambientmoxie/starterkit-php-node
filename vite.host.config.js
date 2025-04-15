69e925ba60b03e03a847c661c26e30c3a3c5bd90
ambientmoxie <contact@maximebenoit.work>
2025-04-15 09:11:52 +0200

    Polish: vite config files

--- vite.host.config.js
+++ vite.host.config.js
@@ -5,19 +5,6 @@ dotenv.config();
 
 export default defineConfig({
   root: ".",
-  base: process.env.VITE_DEV == "true" ? "" : "/assets/bundle",
-  build: {
-    rollupOptions: {
-      input: "./assets/js/main.js",
-      output: {
-        entryFileNames: "[name]-[hash].js",
-        assetFileNames: "[name]-[hash][extname]",
-      },
-    },
-    manifest: true,
-    outDir: "./assets/bundle",
-    emptyOutDir: true,
-  },
   server: {
     host: true,
     origin: `http://${process.env.VITE_DEV}:3000`,
--- vite.local.config.js
+++ vite.local.config.js
@@ -5,23 +5,10 @@ dotenv.config();
 
 export default defineConfig({
   root: ".",
-  base: process.env.VITE_DEV == "true" ? "" : "/assets/bundle",
-  build: {
-    rollupOptions: {
-      input: "./assets/js/main.js",
-      output: {
-        entryFileNames: "[name]-[hash].js",
-        assetFileNames: "[name]-[hash][extname]",
-      },
-    },
-    manifest: true,
-    outDir: "./assets/bundle",
-    emptyOutDir: true,
-  },
   server: {
     host: "localhost",
     origin: "http://localhost:3000",
     port: 3000,
-    hot: true
-  }
+    hot: true,
+  },
 });
